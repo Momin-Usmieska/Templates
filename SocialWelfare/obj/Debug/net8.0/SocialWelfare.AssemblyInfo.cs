9e29d786e2f88322f0dfa81150dbbc002cdc5b30
Momin Rather <momin.rather@gmail.com>
2024-06-21 19:32:12 +0530

    Don't Know

--- SocialWelfare/obj/Debug/net8.0/SocialWelfare.AssemblyInfo.cs
+++ SocialWelfare/obj/Debug/net8.0/SocialWelfare.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SocialWelfare")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f92eea527064204b39c076d8d389916cc9e79af9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7dd37594032527c85047c769201bccc197cd6ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("SocialWelfare")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SocialWelfare")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
